9fdb202fbcbd3b8a5f8d4e1c7879777c53ee4bed
Tommy Pratama <tommyhpratama@gmail.com>
2020-12-10 08:25:02 +0000

    :lipstick: Add new components

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Tommy Pratama
+          </NavLink>
+          <NavLink to="/post">Blog Posts</NavLink>
+          <NavLink to="/project">Projects</NavLink>
+          <NavLink to="/about">About Me!</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
