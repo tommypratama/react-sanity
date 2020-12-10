d4d579b6b089e2cb6f093db7f9b4e32a3f5e3947
Tommy Pratama <tommyhpratama@gmail.com>
2020-12-10 08:22:50 +0000

    :heavy_plus_sign: Add React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-4xl text-red-500">Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
