7a7d175baad7d0726ad4c64179910aadcc0cd43d
Tommy Pratama <tommyhpratama@gmail.com>
2020-12-11 17:30:26 +0000

    :lipstick: Styling navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,18 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
-            Tommy Pratama
-          </NavLink>
-          <NavLink to="/post">Blog Posts</NavLink>
-          <NavLink to="/project">Projects</NavLink>
-          <NavLink to="/about">About Me!</NavLink>
+      <div className="py-7 px-5 border-b">
+        <nav className="flex justify-between items-center max-w-screen-md mx-auto">
+          <div>
+            <NavLink className="text-xl" to="/" exact>
+              Tommy Pratama
+            </NavLink>
+          </div>
+          <div>
+            <NavLink className="hover:bg-gray-100 px-4 py-3 mr-4 rounded" to="/post">Blog Posts</NavLink>
+            <NavLink className="hover:bg-gray-100 px-4 py-3 mr-4 rounded" to="/project">Projects</NavLink>
+            <NavLink className="hover:bg-gray-100 px-4 py-3 rounded" to="/about">About Me!</NavLink>
+          </div>
         </nav>
       </div>
     </header>
